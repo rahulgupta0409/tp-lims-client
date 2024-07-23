300aff7cf3aa2d83cdf0389bdf08a8006004db3c
rahul0409 <rahulguptaharsh081218@gmail.com>
2024-07-23 21:26:04 +0530

    "first commit"

---
+++ src/components/PatientsEntryForm.js
@@ -0,0 +1,23 @@
+import React from 'react'
+import './PatientsEntryForm.scss';
+
+const PatientsEntryForm = () => {
+  return (
+    <div className='patient-form'>
+
+        <p className='patient-entry-header'>Patient Entry</p>
+
+        <form>
+        <label>Enter your name:
+        <input 
+          
+      />
+      </label>
+        </form>
+
+      
+    </div>
+  )
+}
+
+export default PatientsEntryForm
